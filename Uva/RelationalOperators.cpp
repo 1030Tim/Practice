beabb0f1b6d97327812eeca0bd3a36cb8e719eeb
juhn_chen <126213720+1030Tim@users.noreply.github.com>
2025-01-09 21:48:42 +0800

    cpp

--- Uva/RelationalOperators.cpp
+++ Uva/RelationalOperators.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
+
 int main(){
     int n;
     cin >> n;
     for (int i = 0 ; i<n ; i++){
         long long a,b;
         cin >> a >> b;
-        if (a>b) cout<< ">" <<"\n";
-        else if (a<b) cout << "<" << "\n";
-        else cout << "=" << "\n";
+
+        if (a>b) cout <<">";
+        else if (a<b) cout <<"<";
+        else cout <<"=";
+        cout << "\n";
     }
+    return 0;
 }
+
